3a090bbc9ad37afb72c11a268701a5c126d5197a
Kieran Cairney <kierancairney@hotmail.com>
2016-12-15 20:34:50 -0500

    init xcode proj

---
+++ Red&Blue/Red&Blue/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Red&Blue
+//
+//  Created by Kieran on 2016-12-15.
+//  Copyright © 2016 Kieran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
